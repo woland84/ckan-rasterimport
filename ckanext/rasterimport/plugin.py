dd3416c5cdc687499c86c1eebedebb0a8518a8cc
Alex Dumitru <dumitru@rasdaman.com>
2014-10-31 15:33:21 +0100

    remove old ckan files

--- ckanext/rasterimport/plugin.py
+++ ckanext/rasterimport/plugin.py
@@ -1,6 +1,4 @@
 import ckan.plugins as plugins
-import wcst
-from raster_plugin_util import RasterUtil
 
 
 class RasterImportPlugin(plugins.SingletonPlugin):
@@ -9,45 +7,24 @@ class RasterImportPlugin(plugins.SingletonPlugin):
     This class implements the notify method that determines if a dataset was modified and takes appropiate action
     by either registering the raster data or removing it through WCST
     """
-    plugins.implements(plugins.IDomainObjectModification)
+    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IConfigurer, inherit=True)
+    plugins.implements(plugins.IConfigurable, inherit=True)
 
-    def notify(self, entity, operation):
-        print operation
-        if operation == "changed":
-            if entity.as_dict()['state'] == "deleted":
-                pass
-        elif operation == "deleted":
-            if entity.as_dict()['package_id'] is not None:
-                self.delete_coverage(entity.as_dict()['package_id'])
-        elif operation == "new":
-            if entity.as_dict()['url'] is not None:
-                self.insert_coverage(entity.as_dict()['url'], entity.as_dict()['package_id'])
-
-    @staticmethod
-    def insert_coverage(coverage_url, dataset_id):
+    def before_map(self, map):
         """
-        Inserts a coverage attached to a dataset into the WCST service
-
-        :param coverage_url: string - url to the coverage to be inserted (can be in any gdal supported format + GML)
-        :param dataset_id: string - the id of the dataset to which this coverage is attached to
+        Adds the url mappings to the raster import controller
         """
-        coverage_local_path = RasterUtil.get_gml_path_from_dataset(coverage_url, dataset_id)
-        request = wcst.WCSTInsertRequest(coverage_local_path)
-        executor = wcst.WCSTExecutor(RasterUtil.get_wcst_url())
-        executor.execute(request)
+        map.connect("raster-publish", "/api/raster/publish/{resource_id}",
+                    controller='ckanext.rasterimport.controllers.import:RasterImportController', action='publish',
+                    resource_id='{resource_id}')
+        map.connect("raster-delete", "/api/raster/delete/{resource_id}",
+                    controller='ckanext.rasterimport.controllers.import:RasterImportController', action='delete',
+                    resource_id='{resource_id}')
+        return map
 
-    @staticmethod
-    def delete_coverage(dataset_id):
+    def update_config(self, config):
         """
-        Deletes a coverage attached to a dataset from the WCST service
-        :param dataset_id: string - the id of the dataset to which this coverage is attached to
+        Exposes the public folder of this extension. The intermediary gml files will be stored here.
         """
-        request = wcst.WCSTDeleteRequest(RasterUtil.dataset_id_to_coverage_id(dataset_id))
-        executor = wcst.WCSTExecutor(RasterUtil.get_wcst_url())
-        executor.execute(request)
-
-
-
-
-
-
+        plugins.toolkit.add_public_directory(config, 'public')
